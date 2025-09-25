4848b5efbc536d5149c57cac284082bc599cf306
enderdev-v <endercrackgamer@outlook.com>
2025-09-24 21:11:52 -0600

    Add mini project 20 and gitignore :D

---
+++ src/Projects 20-21/20.Project/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+    int n1, n2, n3, aux; // n1 < n2 < n3
+    printf("Enter three integers: \n"); // <-- tipo lo verde es string pero definirlo como char xd[] 
+
+    scanf("%d%d%d", &n1, &n2, &n3);
+    
+    printf("The Original input %d %d %d\n", n1, n2, n3);
+    
+    if (n1 > n2) {
+        aux = n1;
+        n1 = n2;
+        n2 = aux;
+    }
+    if (n2 > n3) {
+        aux = n2;
+        n2 = n3;
+        n3 = aux;
+    }
+    if (n1 > n2) {
+        aux = n1;
+        n1 = n2;
+        n2 = aux;
+    }
+    
+    
+    printf("The final result is  %d %d %d\n", n1, n2, n3);
+    
+
+    int continueProgram = 1;
+
+    printf("Do you want to continue? (1 for yes, 0 for no): \n ");
+
+    scanf("%d", &continueProgram);
+    if (continueProgram == 0) return 0;
+    return main();
+}
